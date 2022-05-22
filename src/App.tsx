93b57545364e58edcf3e011620089efdc1fe7d5f
MierzwaMatheus <mierzwa.oliveira@gmail.com>
2022-05-21 21:37:39 -0300

    Criação de sala adm e exibição da lista de perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+import { AdmRoom } from './pages/AdmRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext'
 
@@ -16,6 +17,8 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/room/new" element={<NewRoom/>} />
           <Route path="/room/:id" element={<Room/>}></Route>
+
+          <Route path="/admin/room/:id" element={<AdmRoom />}/>
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
